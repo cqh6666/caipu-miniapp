a54cfffcc6070c84b8d0843cd689ab0071d80d31
alexchan <2018ch@m.scnu.edu.cn>
2026-04-02 00:47:01 +0800

    Add library header section component

---
+++ pages/index/components/library-header-section.vue
@@ -0,0 +1,101 @@
+<template>
+	<view>
+		<view class="page-header" :class="{ 'page-header--meal-order': isLibraryMealOrderMode }">
+			<view class="page-header__top">
+				<view class="page-header__heading">
+					<view class="page-header__title-row">
+						<view
+							class="page-header__title-mark"
+							:class="isLibraryMealOrderMode ? 'page-header__title-mark--meal-order' : 'page-header__title-mark--library'"
+						>
+							<up-icon
+								:name="isLibraryMealOrderMode ? 'heart-fill' : 'grid-fill'"
+								size="14"
+								:color="isLibraryMealOrderMode ? '#bf715f' : '#7a6755'"
+							></up-icon>
+						</view>
+						<text class="page-header__title">{{ libraryHeaderTitle }}</text>
+					</view>
+					<text v-if="libraryHeaderSummary" class="page-header__summary">{{ libraryHeaderSummary }}</text>
+				</view>
+				<view v-if="isLibraryMealOrderMode" class="meal-order-mode-bar__actions page-header__mode-actions">
+					<view class="meal-order-mode-bar__chip meal-order-mode-bar__chip--accent" @tap="$emit('open-meal-order-date-sheet')">
+						<text class="meal-order-mode-bar__chip-text">改日期</text>
+					</view>
+					<view class="meal-order-mode-bar__chip meal-order-mode-bar__chip--ghost" @tap="$emit('exit-meal-order-mode')">
+						<text class="meal-order-mode-bar__chip-text">返回</text>
+					</view>
+				</view>
+				<view v-else class="page-header__action" @tap="$emit('open-meal-order-date-sheet')">
+					<up-icon name="calendar" size="15" color="#745742"></up-icon>
+					<text class="page-header__action-text">安排菜单</text>
+				</view>
+			</view>
+		</view>
+
+		<view
+			v-if="!isLibraryMealOrderMode"
+			class="meal-order-spotlight"
+			:class="{ 'meal-order-spotlight--empty': !hasMealOrderSpotlightRecord }"
+			@tap="$emit('spotlight-tap')"
+			@touchstart="$emit('spotlight-touchstart', $event)"
+			@touchend="$emit('spotlight-touchend', $event)"
+		>
+			<view class="meal-order-spotlight__main">
+				<text class="meal-order-spotlight__title">{{ mealOrderSpotlightTitle }}</text>
+				<text class="meal-order-spotlight__desc">{{ mealOrderSpotlightDesc }}</text>
+			</view>
+			<view class="meal-order-spotlight__aside">
+				<text v-if="mealOrderSpotlightMetaText" class="meal-order-spotlight__meta-text">{{ mealOrderSpotlightMetaText }}</text>
+				<up-icon name="arrow-right" size="16" color="#8a7968"></up-icon>
+			</view>
+		</view>
+	</view>
+</template>
+
+<script>
+export default {
+	name: 'LibraryHeaderSection',
+	props: {
+		isLibraryMealOrderMode: {
+			type: Boolean,
+			default: false
+		},
+		libraryHeaderTitle: {
+			type: String,
+			default: ''
+		},
+		libraryHeaderSummary: {
+			type: String,
+			default: ''
+		},
+		hasMealOrderSpotlightRecord: {
+			type: Boolean,
+			default: false
+		},
+		mealOrderSpotlightTitle: {
+			type: String,
+			default: ''
+		},
+		mealOrderSpotlightDesc: {
+			type: String,
+			default: ''
+		},
+		mealOrderSpotlightMetaText: {
+			type: String,
+			default: ''
+		}
+	},
+	emits: [
+		'exit-meal-order-mode',
+		'open-meal-order-date-sheet',
+		'spotlight-tap',
+		'spotlight-touchend',
+		'spotlight-touchstart'
+	]
+}
+</script>
+
+<style lang="scss" scoped>
+@import './library-header-section.scss';
+</style>
